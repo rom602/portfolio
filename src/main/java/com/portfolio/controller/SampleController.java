c878afd35db50a81bca55effc928cf992870f25b
KangHyunji <rom602@naver.com>
2018-05-06 00:39:58 +0900

    Hello Spring Boot

--- src/main/java/com/portfolio/controller/SampleController.java
+++ src/main/java/com/portfolio/controller/SampleController.java
@@ -1,4 +1,13 @@
 package com.portfolio.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello Spring Boot!!";
+    }
 }
